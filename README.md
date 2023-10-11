8c9f92fb6f96592c231e0ae96062027a0832daeb
Martin Berg Alstad <me@martials.no>
2023-10-11 19:23:07 +0200

    Flyttet sanity data inn i .env siden det er kun read-only access på publisert data, og man må bruke en API token for å gjøre mer

--- README.md
+++ README.md
@@ -7,11 +7,3 @@
 ```bash
 pnpm install
 ```
-
-### Sett opp .env
-Lag en fil i root av prosjektet som heter `.env.local` og legg til følgende:
-
-```dotenv
-SANITY_PROJECT_ID=DIN_SANITY_PROJECT_ID
-SANITY_DATASET=DIN_SANITY_DATASET
-```
--- sanity.js
+++ sanity.js
@@ -6,8 +6,8 @@ import { createClient } from '@sanity/client'
 // Setter opp Sanity client
 // Queries kan bare kjøres fra server-side komponenter
 export const client = createClient({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
     // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
