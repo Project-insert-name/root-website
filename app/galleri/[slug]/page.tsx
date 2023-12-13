d60580b40cd64fc145ec439a8d7626ac413230d5
simsine <simen@simsine.no>
2023-12-13 14:36:59 +0100

    Added modal, doesnt work

--- app/galleri/[slug]/page.tsx
+++ app/galleri/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { notFound } from "next/navigation"
 import { getImageGalleryBySlug } from "@/sanity/queries/imageGallery"
 import Gallery, {
@@ -5,6 +7,9 @@ import Gallery, {
     GalleryBackButton,
     GalleryImage,
 } from "@/components/imageGallery/gallery"
+import GalleryModal from "@/components/imageGallery/galleryModal"
+import { useCallback, useState } from "react"
+import { Link } from "@nextui-org/react"
 
 interface Params {
     slug: string
@@ -20,15 +25,28 @@ const GalleryPage: AsyncPage<Params> = async ({ params }) => {
 
     if (!imageGallery) return notFound()
 
+    const [isOpen, setIsOpen] = useState(false)
+    const [activeImage, setActiveImage] = useState(undefined)
+
+    const handleImageClick = useCallback(image => {
+        setActiveImage(image)
+        setIsOpen(true)
+    }, [])
+
     return (
-        <Gallery heading={imageGallery.title} event={imageGallery.event}>
-            <GalleryBackButton />
-            {imageGallery.images?.map(image => (
-                <GalleryItem key={image._key}>
-                    <GalleryImage image={image} alt={image.alt} />
-                </GalleryItem>
-            ))}
-        </Gallery>
+        <>
+            <GalleryModal isOpen={isOpen} activeImage={activeImage}></GalleryModal>
+            <Gallery heading={imageGallery.title} event={imageGallery.event}>
+                <GalleryBackButton />
+                {imageGallery.images?.map(image => (
+                    <GalleryItem key={image._key}>
+                        <Link onClick={() => handleImageClick(image)}>
+                            <GalleryImage image={image} alt={image.alt} />
+                        </Link>
+                    </GalleryItem>
+                ))}
+            </Gallery>
+        </>
     )
 }
 
