20e37803314690c166a2e6196f3e67af11073eea
olav223 <steinolav98@hotmail.no>
2024-01-13 01:14:24 +0100

    Feil i prioritet fix

--- sanity/queries/omOss.ts
+++ sanity/queries/omOss.ts
@@ -2,7 +2,7 @@ import {client} from "@/sanity/lib/client";
 import {InfoSide} from "@/sanity/types";
 
 export async function getAllInfoPages(): Promise<ReadonlyArray<InfoSide>>{
-    return await client.fetch('*[_type == "info_sider"] | order(priority) | order(title)')
+    return await client.fetch('*[_type == "info_sider"] | order(title) | order(priority) ')
 }
 
 export async function getAllInfoPageTitles(): Promise<ReadonlyArray<String>>{
