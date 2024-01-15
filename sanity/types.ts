f868c151b4f9c6a41099db56db2f0996561b0461
olav223 <steinolav98@hotmail.no>
2024-01-15 14:32:00 +0100

    Kommentert ut typer for å fix build

--- sanity/types.ts
+++ sanity/types.ts
@@ -51,11 +51,11 @@ export type JobAdvert = SanityValues["job_advert"] & {
     description: MarkdownString
 }
 
-export type Styre = SanityValues["styrer"]
+//export type Styre = SanityValues["styrer"]
 
-export type StyreMedlem = SanityValues["styremedlem"]
+//export type StyreMedlem = SanityValues["styremedlem"]
 
-export type StyreRoller = SanityValues["styre_roller"]
+//export type StyreRoller = SanityValues["styre_roller"]
 
 export type InfoSide = SanityValues["info_sider"] & {
     info: MarkdownString
