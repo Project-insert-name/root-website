449607cd7885a3cd1e4126369e40345334753877
martin <me@martials.no>
2024-01-28 20:49:03 +0100

    Fjernet deprecated css farger

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,18 +18,10 @@ const config: Config = {
     theme: {
         extend: {
             colors: {
-                // @depricated
-                rootBlue: "#1370a6",
-                // @depricated
-                rootBlueDark: "#0B4B6A",
                 "root-primary": "#1370A6",
                 "root-primary-dark": "#0B4B6A",
                 "root-secondary": colors.blue[500],
-                // @depricated
-                defaultBg: "#F3F3F3",
                 "default-background": "#F3F3F3",
-                // @depricated
-                darkTitle: "#3E3E3E",
                 "dark-title": "#3E3E3E",
                 bedpress: "#FFBA09",
                 social: "#09B5FF",
