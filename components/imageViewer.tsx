48d2b94816f5b342c2d09617bfa9f9a0b066a634
Stein Olav Frey <steinolav98@hotmail.no>
2024-02-12 16:52:00 +0100

    Endret slik at man kan se hele bilde ved å klikke på det.

---
+++ components/imageViewer.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import useToggle from "@/hooks/useToggle"
+import SanityImage from "@/components/sanityImage"
+import type { SanityImageObject } from "@/sanity/types"
+import { getImageDimensions } from "@sanity/asset-utils"
+import { urlFor } from "@/sanity/lib/image"
+import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from "@heroicons/react/24/outline"
+
+const ImageViewer = ({ image }: { image?: SanityImageObject }) => {
+    const [openImage, toggleOpenImage] = useToggle()
+
+    const showFullImage = (image ? getImageDimensions(urlFor(image).url()).aspectRatio : 0) > 16 / 7
+
+    return (
+        <div>
+            {image ? (
+                showFullImage ? (
+                    <SanityImage image={image} alt={image.alt} className={"w-full object-cover"} />
+                ) : (
+                    <div>
+                        <button onClick={() => toggleOpenImage()} className={"relative w-full"}>
+                            <SanityImage
+                                image={image}
+                                alt={image.alt}
+                                className={`block ${
+                                    openImage ? "" : "relative aspect-[16/7]"
+                                } w-full object-cover`}
+                            />
+                            <div
+                                className={
+                                    "absolute bottom-2 right-2 rounded-3xl bg-white p-1 opacity-50"
+                                }>
+                                {openImage ? (
+                                    <ArrowsPointingInIcon className={"size-7"} />
+                                ) : (
+                                    <ArrowsPointingOutIcon className={"size-7"} />
+                                )}
+                            </div>
+                        </button>
+                    </div>
+                )
+            ) : null}
+        </div>
+    )
+}
+
+export default ImageViewer
