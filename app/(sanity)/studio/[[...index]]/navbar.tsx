b70cbb20f4becf92c4cc4e9ebaa2761521c29a97
Martin Berg Alstad <emberal+github@pm.me>
2024-05-15 13:07:42 +0200

    Endret icon og skrevet root ved for å være tydelig

--- app/(sanity)/studio/[[...index]]/navbar.tsx
+++ app/(sanity)/studio/[[...index]]/navbar.tsx
@@ -1,21 +1,30 @@
 "use client"
 
 import { type ToolMenuProps } from "sanity"
-import { HomeIcon } from "@heroicons/react/24/outline"
 import { defaultIconSize } from "@/components/icons/icon"
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline"
 
 /**
  * En custom toolmenu for sanity studio. Som legger til en lenke tilbake root-nettsiden.
  * @param props Props som sendes fra sanity studio.
  */
 const SanityToolMenu: Component<ToolMenuProps> = props => (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", alignItems: "center" }}>
         <a
             href={process.env.NEXT_PUBLIC_BASE_URL}
             rel={"noopener"}
             target={"_blank"}
+            style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "5px",
+                color: "white",
+                textDecoration: "none",
+                fontSize: "small",
+            }}
             title={"Tilbake til Root"}>
-            <HomeIcon width={defaultIconSize} style={{ color: "white" }} />
+            <ArrowTopRightOnSquareIcon width={defaultIconSize} />
+            <p>Root</p>
         </a>
         <div style={{ width: "100%" }}>{props.renderDefault(props)}</div>
     </div>
