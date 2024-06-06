16caeca266d76ae32ca465520ba001b7b6ccfa82
Stein Olav Løset Frey <steinolav98@hotmail.no>
2024-06-06 21:46:48 +0200

    Change to h3 from h6 for accessibility reasons

--- components/cards/eventCard.tsx
+++ components/cards/eventCard.tsx
@@ -82,7 +82,7 @@ export const SingleEventWide: Component<RootEvent & DefaultProps> = ({
             <EventMarker type={type} />
             <div>
                 <Link href={`arrangement/${slug.current}`}>
-                    <h6 className={"text-root-primary dark:text-root-light"}>{title}</h6>
+                    <h3 className={"text-base text-root-primary dark:text-root-light"}>{title}</h3>
                 </Link>
                 <p>{getEventTypeLabel(type)}</p>
                 <div className={"flex flex-col gap-2 sm:flex-row"}>
@@ -118,7 +118,7 @@ export const SingleEventNarrow: Component<RootEvent & DefaultProps> = ({
 }) => (
     <div className={`mx-1 my-3 flex w-full flex-col ${className}`}>
         <Link href={`arrangement/${slug.current}`}>
-            <h6 className={"text-root-primary dark:text-root-light"}>{title}</h6>
+            <h3 className={"text-base text-root-primary dark:text-root-light"}>{title}</h3>
         </Link>
         <div className={"inline-flex justify-between"}>
             <div>
